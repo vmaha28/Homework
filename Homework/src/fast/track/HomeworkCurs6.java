6fb81cec4743547277a38e33ded3652b704e2809
Vlad <vlad.mahalean@gmail.com>
2022-06-04 13:14:10 +0300

    Homework Done

--- Homework/src/fast/track/HomeworkCurs6.java
+++ Homework/src/fast/track/HomeworkCurs6.java
@@ -12,7 +12,8 @@ public class HomeworkCurs6 {
         //Ex2();
         //Ex3();
         //Ex4(80);
-        Ex5(100, 1);
+        //Ex5(100, 1);
+        Ex6("Ana are mere.Vlad e baiat.Marius e un nume random.");
 
 
     }
@@ -83,8 +84,16 @@ public class HomeworkCurs6 {
                 totalDonatii += r.nextInt(donatii);
             }
         }
-        System.out.println("S-a strans suma de " + totalDonatii + " ,asta este");
-
+        System.out.println("S-a strans suma de " + totalDonatii);
+        if (totalDonatii < donatii) System.out.println("Asta este, a fost o campanie ineficienta");
 
     }
+    public static void Ex6(String string){
+        String[] stringArray = string.split("\\.");
+        for(int i=0; i< stringArray.length; i++)
+            System.out.println(stringArray[i]);
+    }
+
+
+
 }
