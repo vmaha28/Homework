1a59fc9a3f6be67348eeb7dcfb5a7d546de644de
Vlad <vlad.mahalean@gmail.com>
2022-06-04 11:24:20 +0300

    Ex1+Ex2+Ex3 Done

--- Homework/src/fast/track/HomeworkCurs6.java
+++ Homework/src/fast/track/HomeworkCurs6.java
@@ -1,28 +1,64 @@
 package fast.track;
 
-import java.sql.SQLOutput;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class HomeworkCurs6 {
     public static void main(String[] args) {
-        Ex1();
-
+        //Ex1();
+        //Ex2();
+        Ex3();
 
 
     }
-    public static void Ex1(){
+
+    public static void Ex1() {
         List<Integer> list = new ArrayList<>();
         list.add(0, 2);
         list.add(4);
         list.add(5);
         list.add(8);
-        int sum= 0;
+        int sum = 0;
         for (int i = 0; i < list.size(); i++) {
-            sum +=list.get(i);
+            sum += list.get(i);
             System.out.println(sum);
         }
+
+    }
+
+    public static void Ex2() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(8);
+        list.add(4);
+        list.add(6);
+        list.add(9);
+        list.add(11);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 1) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+
+    public static void Ex3() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(8);
+        list.add(4);
+        list.add(6);
+        list.add(9);
+        list.add(11);
+        int a = 5;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > a) {
+                System.out.println(list.get(i));
+            }
+        }
+
+
 
     }
 }
